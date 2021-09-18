c57610389f46925815a5e91509a06ecbdf5c0716
ltylover <1192588363@qq.com>
2021-09-18 17:27:38 +0800

    change WiFiserver to webserver

--- ArduinoIDE/webServers/webServers.ino
+++ ArduinoIDE/webServers/webServers.ino
@@ -1,43 +1,49 @@
-#include <ETH.h>
 #include <WiFi.h>
-#include <WiFiAP.h>
 #include <WiFiClient.h>
-#include <WiFiGeneric.h>
-#include <WiFiMulti.h>
-#include <WiFiScan.h>
 #include <WiFiServer.h>
-#include <WiFiSTA.h>
-#include <WiFiType.h>
-#include <WiFiUdp.h>
+#include <WebServer.h>
 
 String led1State = "OFF";
 String receline = "";
 
-const char * ssid = "ESP32";
-const char * password = "12345678";
+const char* ssid = "ESP32";
+const char* password = "12345678";
 
-WiFiServer server(80);
-WiFiClient newClient;
+//WiFiServer server(80);
+//WiFiClient newClient;
 
-void pageDisplay(){
-  newClient.println("HTTP/1.1 200 OK");
-  newClient.println("Connect - type:text/html");
-  newClient.println();
-  newClient.println("<! DOCTYPE HTML >");
-  newClient.println("< html >");
-  newClient.println("< head >");
-  newClient.println("< title >ESP32 Web Server </title>");
-  newClient.println("< meta charset = \"utf-8\">");
-  newClient.println("< style > html{text-align:center;}</style> ");
-  newClient.println("< /head >");
-  newClient.println("< body >");
-  newClient.println("< h2 >ESP32 Web Server </h2 >");
-  newClient.println("< p >GPIO 4 - " + led1State + "< /p >");
-  newClient.println("< p > < a href =\"/led1_on\"> < button > 打开 < /button > < /a > < /p >");
-  newClient.println("< p > < a href =\"/led1_off\"> < button > 关闭 < /button > < /a > < /p >"); 
-  newClient.println("< /body > < /html >");
-  newClient.println();  
-}
+WebServer server(80);  
+
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<meta charset = \"utf-8\">\
+<h1>ESP32 Web Server </h1>\
+<p>GPIO 4 - " + led1State + "</p>\
+<p><a href =\"/led1_on\"><button>打开</button></a></p>\
+<p><a href =\"/led1_off\"><button>关闭</button></a></p>\
+</body>\
+</html>";
+
+//void pageDisplay(){
+//  newClient.println("HTTP/1.1 200 OK");
+//  newClient.println("Connect - type:text/html");
+//  newClient.println();
+//  newClient.println("<! DOCTYPE html >");
+//  newClient.println("< html >");
+//  newClient.println("< head >");
+//  newClient.println("< title >ESP32 Web Server </title>");
+//  newClient.println("< meta charset = \"utf-8\">");
+//  newClient.println("< style > html{text-align:center;}</style> ");
+//  newClient.println("< /head >");
+//  newClient.println("< body >");
+//  newClient.println("< h2 >ESP32 Web Server </h2 >");
+//  newClient.println("< p >GPIO 4 - " + led1State + "< /p >");
+//  newClient.println("< p > < a href =\"/led1_on\"> < button > 打开 < /button > < /a > < /p >");
+//  newClient.println("< p > < a href =\"/led1_off\"> < button > 关闭 < /button > < /a > < /p >"); 
+//  newClient.println("< /body > < /html >");
+//  newClient.println();  
+//}
 
 void setup() {
   Serial.begin(115200);
@@ -48,34 +54,47 @@ void setup() {
   Serial.print("ESP32 AP IP address:  ");
   Serial.println(myIP);
   Serial.println();
+
+  server.on("/", handle_root);
+  
   server.begin();
+  Serial.println("HTTP server started");
+  delay(100);
 }
 
 void loop() {
-  newClient = server.available();
-  if(newClient){
-    Serial.print("new Client connected. IP address is : ");
-    Serial.println(newClient.remoteIP());
+//  newClient = server.available();
+//  if(newClient){
+//    Serial.print("new Client connected. IP address is : ");
+//    Serial.println(newClient.remoteIP());
+//
+//    while(newClient.connected()){
+//      if(newClient.available()>0){
+//        char c =newClient.read();
+//        Serial.print(c);
+//        if(c == '\n' ){
+//          if(receline.length() == 0){
+//            pageDisplay();
+//            break;
+//          }
+//          else{
+//            receline = "";
+//          }
+//        }
+//        else if( c != '\r'){
+//          receline += c;
+//        }
+//      }
+//    }
+//    newClient.stop();
+//    Serial.println("Client disconnected.");
+//  }
+   /********/ 
+   server.handleClient();
 
-    while(newClient.connected()){
-      if(newClient.available()>0){
-        char c =newClient.read();
-        Serial.print(c);
-        if(c == '\n' ){
-          if(receline.length() == 0){
-            pageDisplay();
-            break;
-          }
-          else{
-            receline = "";
-          }
-        }
-        else if( c != '\r'){
-          receline += c;
-        }
-      }
-    }
-    newClient.stop();
-    Serial.println("Client disconnected.");
-  }
+}
+
+// Handle root url (/)
+void handle_root() {
+  server.send(200, "text/html", HTML);
 }
