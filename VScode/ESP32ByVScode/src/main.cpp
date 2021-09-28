11f812cd58ccfbdde8b6104ede2abe5955b877a0
ltylover <1192588363@qq.com>
2021-09-28 10:45:16 +0800

    attachInterrupt  function

--- VScode/ESP32ByVScode/src/main.cpp
+++ VScode/ESP32ByVScode/src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 
+#define EXT_TSR_MODE 0
+//定义外部中断的Mode模式
+
+void interruptEvent()
+{
+  digitalWrite(2,HIGH);
+}
+//中断事件内容
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+
+#if 0 == EXT_TSR_MODE
+  pinMode(2,OUTPUT);
+  pinMode(0,INPUT_PULLUP);
+  attachInterrupt(0,interruptEvent,FALLING);
+    //触发中断事件
+#elif 1 == EXT_TSR_MODE
+  
+#endif
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  #if 0 == EXT_TSR_MODE
+    //触发中断事件
+  #elif 1 == EXT_TSR_MODE
+
+  #endif
+
+  digitalWrite(2,HIGH);
+
 }
