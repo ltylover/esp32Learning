8012c56f4135f9caea363b8c94dfc9811bc8cb9a
ltylover <1192588363@qq.com>
2021-09-28 14:18:11 +0800

    change pin connect,pin 34 35 36 39 only INPUT,can drive led1602

--- VScode/ESP32ByVScode/src/main.cpp
+++ VScode/ESP32ByVScode/src/main.cpp
@@ -12,20 +12,20 @@ void interruptEvent()
 void setup() {
   Serial.begin(115200);
 
-  #if 0 == EXT_TSR_MODE
+  #if 1 == EXT_TSR_MODE
   pinMode(2,OUTPUT);
   pinMode(0,INPUT_PULLUP);
   attachInterrupt(0,interruptEvent,FALLING);//(pin,中断服务函数，IO口引起的电平类型)
     //触发中断事件
-  #elif 1 == EXT_TSR_MODE
+  #elif 2 == EXT_TSR_MODE
   
   #endif
 }
 
 void loop() {
-  #if 0 == EXT_TSR_MODE
+  #if 1 == EXT_TSR_MODE
     //触发中断事件
-  #elif 1 == EXT_TSR_MODE
+  #elif 2 == EXT_TSR_MODE
 
   #endif
 
