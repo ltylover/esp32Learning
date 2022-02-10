5f5138f0600873b3b00f77ba41167ce61731d7b8
ltylover <1192588363@qq.com>
2022-02-10 22:14:40 +0800

    引发中断的端口要上拉或下拉输入，中断函数里不能有延时，这些都会导致重启，但现在不能按键消抖

--- ArduinoIDE/interruptTest/interruptTest.ino
+++ ArduinoIDE/interruptTest/interruptTest.ino
@@ -1,18 +1,14 @@
 #define sw1 13
 #define led1 2
 
-int ledStatus = 0;
-
 void IRAM_ATTR ZD() {
-  if(ledStatus % 2 == 0){
+  if(digitalRead(led1) == LOW){
     digitalWrite(led1,HIGH);
     Serial.println("led1 ON");
-    ledStatus = 1;
   }
   else{
     digitalWrite(led1,LOW);
     Serial.println("led1 OFF");
-    ledStatus = 0;
   }
 }
 
@@ -20,11 +16,11 @@ void setup() {
   Serial.begin(115200);
   Serial.println("led1 SET");
   pinMode(led1,OUTPUT);
-  pinMode(sw1,INPUT);
+  pinMode(sw1,INPUT_PULLUP);
   digitalWrite(led1,LOW);
   attachInterrupt(sw1,ZD,FALLING);
 }
-
 void loop() {
   Serial.println("led1 WAIT");
+  delay(100);
 }
