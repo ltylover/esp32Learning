7ddaf13461908f28c723d2826c837f32fd9c2dad
ltylover <1192588363@qq.com>
2022-02-13 15:04:27 +0800

    改了oled的通讯方式，变成了IIC，仍然不能点亮

--- ArduinoIDE/oledSSD1306HelloWorld/oledSSD1306HelloWorld.ino
+++ ArduinoIDE/oledSSD1306HelloWorld/oledSSD1306HelloWorld.ino
@@ -1,17 +1,22 @@
-#include <OLEDDisplay.h>
-#include <OLEDDisplayFonts.h>
-#include <OLEDDisplayUi.h>
-#include <SSD1306Spi.h>
+#include "SSD1306.h"
 
 #include <Wire.h>
 
-SSD1306Spi display(0, 2, 14);  // RES, DC, CS
+SSD1306 display(0x7A, 21, 22); 
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(14,OUTPUT);
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH);
+  digitalWrite(14,LOW);
+  delay(500);
+  digitalWrite(14,HIGH);
   display.init();
 
   display.setFont(ArialMT_Plain_24);
   display.drawString(0,0,"hello world!");
+  Serial.println("hello world!");
   display.display();
   
 }
